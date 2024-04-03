5d53897a84d9436eb1ab50f735eeae9e569f380e
ricsjs <ricfilho0007@gmail.com>
2024-04-03 15:08:53 -0300

    feat: implementing get attendees by event_id

--- src/main/routes/attendee_routes.py
+++ src/main/routes/attendee_routes.py
@@ -19,3 +19,11 @@ def get_attendees_badge(attendee_id):
   
   http_response = attendees_handler.find_attendee_badge(http_request)
   return jsonify(http_response.body), http_response.status_code
+
+@attendee_route_bp.route("/events/<event_id>/attendees", methods=["GET"])
+def get_attendees(event_id):
+  attendees_handler = AttendeeHandler()
+  http_request = HttpRequest(param={ "event_id": event_id })
+  
+  http_response = attendees_handler.finds_attendees_from_event(http_request)
+  return jsonify(http_response.body), http_response.status_code
