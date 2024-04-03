ac1bf33b76058da3a07e5419391b5c301f33b806
ricsjs <ricfilho0007@gmail.com>
2024-04-03 14:53:03 -0300

    feat: implementing AttendeesHandler with attendee badge route

---
+++ src/main/routes/attendee_routes.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, jsonify, request
+from src.http_types.http_request import HttpRequest
+from src.data.attendee_handler import AttendeeHandler
+
+attendee_route_bp = Blueprint("attendee_route", __name__)
+
+@attendee_route_bp.route("/events/<event_id>/register", methods=["POST"])
+def create_attendee(event_id):
+  attendees_handler = AttendeeHandler()
+  http_request = HttpRequest(param={ "event_id": event_id }, body=request.json)
+  
+  http_response = attendees_handler.register(http_request)
+  return jsonify(http_response.body), http_response.status_code
+
+@attendee_route_bp.route("/attendees/<attendee_id>/badge", methods=["GET"])
+def get_attendees_badge(attendee_id):
+  attendees_handler = AttendeeHandler()
+  http_request = HttpRequest(param={ "attendee_id": attendee_id })
+  
+  http_response = attendees_handler.find_attendee_badge(http_request)
+  return jsonify(http_response.body), http_response.status_code
--- src/main/server/server.py
+++ src/main/server/server.py
@@ -8,4 +8,7 @@ app = Flask(__name__)
 CORS(app)
 
 from src.main.routes.event_routes import event_route_bp
+from src.main.routes.attendee_routes import attendee_route_bp
+
 app.register_blueprint(event_route_bp)
+app.register_blueprint(attendee_route_bp)
